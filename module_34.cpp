9c89e1f36508eb2ee9c12e1416b364413f0550ec
dashkov <dashkov-d@yandex.ru>
2023-09-20 15:59:20 +0300

    update

--- module_34.cpp
+++ module_34.cpp
@@ -12,14 +12,14 @@ concept ComplexConcept =
 		{ v.to_string() } -> std::same_as<std::string>;
 	}
 &&
-	std::has_virtual_destructor<T>::value
+	!std::has_virtual_destructor<T>::value
 ;
 
 struct MyStruct
 {
 	int val{0};
 	MyStruct(int v) : val(v) {}
-	virtual ~MyStruct() { std::cout << "destructor working" << std::endl; }
+	//virtual ~MyStruct() { std::cout << "destructor working" << std::endl; }
 	long hash() const { return static_cast<long> (val); }
 	std::string to_string() const { return "this is string"; }
 };
